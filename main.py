b25a63167a219edc02dc55347166a051a5bee4ce
Paul Martini <paul.martini@web.de>
2026-02-10 18:30:38 +0100

    add step-wise examples

--- main.py
+++ main.py
@@ -73,13 +73,295 @@ def workflow_pipeline_unsupervised():
     gp.save(filepath=save_dir)
 
 
-def workflow_manual_unsupervised():
+def workflow_step_wise_unsupervised_som_training():
 
     import os
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
     from flagx.io import FlowDataManager
     from flagx.gating import SomClassifier
-    from flagx.io import export_to_fcs
+
+    # --- Define path where results are saved to
+    save_path = './results/workflow_step_wise_unsupervised_som_training'
+    save_path_data_handling = './results/workflow_step_wise_unsupervised_som_training/data_handling'
+    os.makedirs(save_path_data_handling, exist_ok=True)
+
+    # --- Specify where training data is saved and specify the corresponding filenames
+    # Define path to training data
+    training_data_path = './data/flagx_tutorial_data/training'
+
+    # Get list of files in the data directory (only include ones ending with .csv)
+    training_files = sorted([fn for fn in os.listdir(training_data_path) if fn.endswith('.csv')])
+
+    # --- Before data processing with the FlowDataManager, check whether each file contains the same number of channels
+    # Load the training files into pandas dataframes
+    training_data_dfs = [pd.read_csv(os.path.join(training_data_path, fn)) for fn in training_files]
+
+    # For each file print the number of channels
+    for fn, data_df in zip(training_files, training_data_dfs):
+        print(f'# --- {fn}, number of channels: {data_df.shape[1]}')
+
+    print('\n')
+
+    # As an additional check, also print the channel names
+    for fn, data_df in zip(training_files, training_data_dfs):
+        print(f'# --- {fn}:\n{data_df.columns.to_list()}')
+
+    print('\n')
+
+    # RESULT:
+    #  - Number of channels is consistent across samples
+    #  - If one sample had 14 instead of 13 channels (e.g. FS Peak),
+    #    this channel should be removed to match the other samples.
+
+
+    # --- Data loading and processing
+    # Initialize the data manager
+    fdm = FlowDataManager(
+        data_file_names=training_files,
+        data_file_type=None,  # Is inferred from the filename ending of the 1st file in the 'training_files' list
+        data_file_path=training_data_path,
+        save_path=save_path_data_handling,
+        verbosity=1
+    )
+
+    # Load data into memory
+    # Data samples are now stored not in a Pandas DataFrames, but in a list of AnnData object.
+    # This list is an attribute of the FlowDataManager class and can be accessed via FlowDataManager.anndata_list_.
+    # AnnData is a Python class similar to Pandas DataFrames but with more options and functions for data annotation.
+    # see: https://anndata.readthedocs.io/en/stable/
+    fdm.load_data_files_to_anndata()
+    # Print the loaded AnnData objects containing the loaded training data
+    for adata in fdm.anndata_list_:
+        print(f'# --- {adata.uns['filename']}:\n{adata}')
+
+    # --- Check the number of events per sample
+    # Create a dataframe with the columns sample and n_events, this df is an attribute of the FDM instance
+    # and can be accessed via FDM.sample_sizes_. Since a filename is passed as well, the df is also saved in the
+    # directory specified via 'save_path_data_handling'.
+    fdm.check_sample_sizes(filename_sample_sizes_df='sample_sizes.csv')
+
+    # Use a built-in plotting function to visualize the number of events per sample.
+    # Resulting plot also saved to 'save_path_data_handling'.
+    fig, ax = plt.subplots(dpi=300)
+    fdm.plot_sample_size_df(sample_size_df=fdm.sample_sizes_, ax=ax)
+    fig.savefig(os.path.join(save_path_data_handling, 'sample_sizes.png'))
+    plt.close(fig)
+
+    # --- Apply preprocessing transformation to each sample
+    # Example 1: Apply arcsinh with cofactor 150,
+    # Example 2: Apply log transformation with custom cutoffs
+    # In both cases, store non-transformed data in a separate layer of the AnnData object that we call 'no_trafo'.
+    example_1 = True
+    if example_1:
+        preprocessing_kwargs = {'cofactor': 150}
+        fdm.sample_wise_preprocessing(flavour='arcsinh', save_raw_to_layer='no_trafo', **preprocessing_kwargs)
+    else:
+        # Define python dictionary mapping channel names to cutoffs (arbitrarily chosen here, adjust if needed)
+        channel_name_to_cutoff = {
+            'FS INT': 1000, 'SS INT': 800,
+            '15-FITC': 300, '13-PE': 300, '34-ECD': 300, '117-PC5.5': 300, '33-PC7': 300,
+            '2-APC': 200, '7-APC-AF700': 200, '-APC-AF750': 200, 'HLADR-PB': 200, '45-CO': 200,
+        }
+        preprocessing_kwargs = {'cutoffs': channel_name_to_cutoff}
+        fdm.sample_wise_preprocessing(
+            flavour='log10_w_custom_cutoffs', save_raw_to_layer='no_trafo', **preprocessing_kwargs
+        )
+
+    # --- Downsample each sample to a target number of events
+    # Set target_num_events to 1000 for fast model training in this example
+    fdm.sample_wise_downsampling(data_set='all', target_num_events=1000)
+
+    # --- Extract concatenated data matrix for model training
+    # Define channels to be used for model training
+    channels = [
+        'FS INT', 'SS INT',
+        '15-FITC', '13-PE', '34-ECD', '117-PC5.5', '33-PC7', '2-APC', '7-APC-AF700', '-APC-AF750', 'HLADR-PB', '45-CO'
+    ]
+    # Extract the processed data matrices from the AnnData objects
+    data_matrices = [adata[:, channels].X for adata in fdm.anndata_list_]
+    # Concatenate
+    x_train = np.concatenate(data_matrices, axis=0)
+    # Shuffle
+    idx_shuffle = np.random.permutation(x_train.shape[0])
+    x_train = x_train[idx_shuffle]
+
+    # --- SOM training
+    # Instantiate the SOMClassifier, set hyperparameters
+    som_clf = SomClassifier(
+        som_topology='planar',
+        som_grid_type='rectangular',
+        som_dimensions=(6, 3),  # (25, 25)
+        neighborhood='gaussian',
+        gaussian_neighborhood_sigma=0.1,
+        initialization='pca',
+        n_epochs=100,  # 1000,
+        radius_0=-0.25,
+        radius_n=0.1,
+        radius_cooling='exponential',
+        learning_rate_0=0.1,
+        learning_rate_n=0.001,
+        learning_rate_decay='exponential',
+        unlabeled_label=-999,
+        verbosity=2
+    )
+
+    # Model fitting. Since no labels are available only the SOM component is trained in an unsupervised fashion.
+    # Still, a dummy label vector must be passed containing the label chosen to indicate unlabeled events
+    y_dummy = np.ones(x_train.shape[0]) * -999
+    som_clf.fit(X=x_train, y=y_dummy)
+
+    # Save the trained model
+    som_clf.save(filename='som_classifier.pkl', filepath=save_path)
+
+
+def workflow_step_wise_unsupervised_inference():
+    import os
+    import numpy as np
+    import pandas as pd
+
+    from flagx.io import FlowDataManager, export_to_fcs
+    from flagx.gating import SomClassifier
+    from flagx.dimred import TSNE, UMAP
+
+    # --- Define path where results are saved to
+    save_path = './results/workflow_step_wise_unsupervised_inference'
+    os.makedirs(save_path, exist_ok=True)
+
+
+    # --- Specify where test data is saved and specify the corresponding filenames
+    # Define path to training data
+    test_data_path = './data/flagx_tutorial_data/testing'
+
+    # Get list of files in the data directory (only include ones ending with .csv)
+    test_files = sorted([fn for fn in os.listdir(test_data_path) if fn.endswith('.csv')])
+
+    # --- Before data processing with the FlowDataManager, check whether each file contains the same number of channels
+    # Load the training files into pandas dataframes
+    test_data_dfs = [pd.read_csv(os.path.join(test_data_path, fn)) for fn in test_files]
+
+    # For each file print the number of channels
+    for fn, data_df in zip(test_files, test_data_dfs):
+        print(f'# --- {fn}, number of channels: {data_df.shape[1]}')
+
+    print('\n')
+
+    # As an additional check, also print the channel names
+    for fn, data_df in zip(test_files, test_data_dfs):
+        print(f'# --- {fn}:\n{data_df.columns.to_list()}')
+
+    print('\n')
+
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # The test data files contain an error:
+    # Channel '-APC-AF750' is named '#NAME?' (probably an artifact from manual renaming of the channels).
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    # Fix the naming error and save the corrected files into a newly created directory
+    # Name the column correctly
+    for data_df in test_data_dfs:
+        data_df.rename(columns={'#NAME?': '-APC-AF750'}, inplace=True)
+        # print(data_df.columns.to_list())
+
+    # Create a new dir, save, and reset test_data_path
+    test_data_path_correct = './data/flagx_tutorial_data/testing_corrected'
+    os.makedirs(test_data_path_correct, exist_ok=True)
+    for fn, data_df in zip(test_files, test_data_dfs):
+        data_df.to_csv(os.path.join(test_data_path_correct, fn), index=False)
+
+
+    # --- Data loading and processing; NOTE: data should be processed the same way as for model training
+    # Initialize the data manager
+    fdm = FlowDataManager(
+        data_file_names=test_files,
+        data_file_type=None,  # Is inferred from the filename ending of the 1st file in the 'test_files' list
+        data_file_path=test_data_path_correct,
+        verbosity=1
+    )
+
+    # Load data into memory
+    fdm.load_data_files_to_anndata()
+
+    # --- Apply preprocessing transformation to each sample, same as for training!
+    # Example 1: Apply arcsinh with cofactor 150,
+    # Example 2: Apply log transformation with custom cutoffs
+    # In both cases, store non-transformed data in a separate layer of the AnnData object that we call 'no_trafo'.
+    example_1 = True
+    if example_1:
+        preprocessing_kwargs = {'cofactor': 150}
+        fdm.sample_wise_preprocessing(flavour='arcsinh', save_raw_to_layer='no_trafo', **preprocessing_kwargs)
+    else:
+        # Define python dictionary mapping channel names to cutoffs (arbitrarily chosen here, adjust if needed)
+        channel_name_to_cutoff = {
+            'FS INT': 1000, 'SS INT': 800,
+            '15-FITC': 300, '13-PE': 300, '34-ECD': 300, '117-PC5.5': 300, '33-PC7': 300,
+            '2-APC': 200, '7-APC-AF700': 200, '-APC-AF750': 200, 'HLADR-PB': 200, '45-CO': 200,
+        }
+        preprocessing_kwargs = {'cutoffs': channel_name_to_cutoff}
+        fdm.sample_wise_preprocessing(
+            flavour='log10_w_custom_cutoffs', save_raw_to_layer='no_trafo', **preprocessing_kwargs
+        )
+
+    # NOTE: typically no downsampling at inference time, do so for faster UMAP and t-SNE computation
+    # --- Downsample each sample to a target number of events
+    fdm.sample_wise_downsampling(data_set='all', target_num_events=100)
+
+    # Define channels that were used for model training
+    channels = [
+        'FS INT', 'SS INT',
+        '15-FITC', '13-PE', '34-ECD', '117-PC5.5', '33-PC7', '2-APC', '7-APC-AF700', '-APC-AF750', 'HLADR-PB', '45-CO'
+    ]
+    # Extract the processed data matrices from the AnnData objects
+    data_matrices = [adata[:, channels].X for adata in fdm.anndata_list_]
+    # Get number of events per test sample and compute indices at which samples start in the concatenated data matrix
+    num_events = [x.shape[0] for x in data_matrices]
+    starting_indices = np.cumsum([0, ] + num_events)
+    # Concatenate
+    x_test = np.concatenate(data_matrices, axis=0)
+
+    # --- Compute dimensionality reductions on concatenated test samples
+    # --- SOM
+    # Load the previously trained SOM model
+    som_clf = SomClassifier.load(
+        filename='som_classifier.pkl',
+        filepath='./results/workflow_step_wise_unsupervised_som_training'
+    )
+    _, x_som, _, _ = som_clf.transform(x_test)
+
+    # --- UMAP
+    umap_model = UMAP(n_components=2, n_jobs=-1)
+    x_umap = umap_model.fit_transform(x_test)
+
+    # --- t-SNE
+    tsne_model = TSNE(n_components=2, n_jobs=-1)
+    x_tsne = tsne_model.fit_transform(x_test)
+
+    # Change back into sample-wise format (input format required by export function)
+    x_soms_1 = [x_som[starting_indices[i]: starting_indices[i + 1], 0] for i in range(len(num_events))]
+    x_soms_2 = [x_som[starting_indices[i]: starting_indices[i + 1], 1] for i in range(len(num_events))]
+    x_umaps_1 = [x_umap[starting_indices[i]: starting_indices[i + 1], 0] for i in range(len(num_events))]
+    x_umaps_2 = [x_umap[starting_indices[i]: starting_indices[i + 1], 1] for i in range(len(num_events))]
+    x_tsnes_1 = [x_tsne[starting_indices[i]: starting_indices[i + 1], 0] for i in range(len(num_events))]
+    x_tsnes_2 = [x_tsne[starting_indices[i]: starting_indices[i + 1], 1] for i in range(len(num_events))]
+
+    # Export to FCS
+    export_to_fcs(
+        data_list=fdm.anndata_list_,  # Export the test samples
+        layer_key='no_trafo',  # We want to export non-transformed data => choose the 'no_trafo' layer
+        sample_wise=False,  # Export one FCS in which the test samples are concatenated
+        add_columns=[
+            x_soms_1, x_soms_2,
+            x_umaps_1, x_umaps_2,
+            x_tsnes_1, x_tsnes_2
+        ],  # Add columns corresponding to the 1st and 2nd dimension of the dimensionality reductions into 2D
+        add_columns_names=['SOM_1', 'SOM_2', 'UMAP_1', 'UMAP_2', 'TSNE_1', 'TSNE_2'],  # Add names for added columns
+        scale_columns=['SOM_1', 'SOM_2', 'UMAP_1', 'UMAP_2', 'TSNE_1', 'TSNE_2'],  # Select added columns for scaling
+        val_range=(0, 2**20),  # Range to which selected columns are scaled to
+        save_path=save_path,
+        save_filenames='annotated_test_data.fcs'
+    )
 
 
 
@@ -90,20 +372,11 @@ def workflow_manual_unsupervised():
 
 if __name__ == '__main__':
 
-    workflow_pipeline_unsupervised()
+    # workflow_pipeline_unsupervised()
 
+    workflow_step_wise_unsupervised_som_training()
 
-
-    # import os
-    # import pandas as pd
-    #
-    # train_path = './data/training'
-    # test_path = './data/test'
-    #
-    # for fn in os.listdir(train_path):
-    #     data = pd.read_csv(os.path.join(train_path, fn))
-    #     data_ds = data.sample(n=100, random_state=42)
-    #     data_ds.to_csv(os.path.join(test_path, fn), index=False)
+    workflow_step_wise_unsupervised_inference()
 
     print('done')
 
