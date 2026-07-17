499ec73e363b3e0e2c5aff8e246fca9c32664b95
Stefan Krause <stefan.w.krause@web.de>
2026-07-17 10:49:03 +0200

    PARC unsupervised clustering included

---
+++ training_from_csv_PARC.py
@@ -0,0 +1,224 @@
+# SOM training (unsupervised) for samples without population annotation. Fast tSNE added. 
+# shuffle version of the training data used for model fitting. Dimensionality reductions and FCS export based on unshuffled data
+# read csv, perform training, dim reduction, export fcs and save models (SOM and optional:  TSNE)
+# Before use, check number and names of channels is consistent across samples (use separate script)
+# check if channels like sample_idx (from manual sampling) are included in the csv and need to be scaled for export (if so, add to 'scale_columns')
+# Sample_id channel is added by script to tag the different files
+# automated clustering by PARC added. tested and running 
+# config parameters moved to yaml file 2026-07-13
+
+import os
+import yaml
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pickle
+import parc
+
+print('loading packages and paths...')
+timestart = datetime.now()
+date_time_str = timestart.strftime("%Y-%m-%d_%Hh%M")
+
+from flagx.io import FlowDataManager, export_to_fcs
+from flagx.gating import SOMClassifier
+from flagx.dimred import UMAP
+from openTSNE import TSNE
+
+# --- selected Parameters for the workflow are drawn from YAML files -------
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config_Bcell.yml')
+with open(config_path, 'r', encoding='utf-8') as f:
+    config = yaml.safe_load(f) or {}
+trainchannels = config.get('trainchannels')
+# List of channels to be used for training. Check spelling and consistency across samples. Adjust if needed.
+# full set for AL1: 'FS INT', 'SS INT', '15-FITC', '13-PE', '33-PC7', '2-APC', '7-APC-AF700', '34-ECD', '117-PC5.5', 'HLADR-PB', '45-CO'
+size_per_sample = config.get('size_per_sample')  # Maximum number of events per sample to be used for model training
+SOM_dim = tuple(config.get('SOM_dim'))  # Dimensions of the SOM grid. 10x10 for fast testing, 25x25 to 30x30 for better resolution
+SOM_epochs = config.get('SOM_epochs')  # Number of epochs for SOM training. default 100 for smaller grids, up to 1000
+val_range_list = config.get('val_range') 
+val_range = tuple(val_range_list)  
+trafo_arcsinh = config.get('trafo_arcsinh')   
+
+# --- Define path where results are saved to
+save_path = './results/workflow_step_wise_unsupervised_som_training'
+save_path_data_handling = './results/workflow_step_wise_unsupervised_som_training/data_handling'
+os.makedirs(save_path_data_handling, exist_ok=True)
+
+# --- Specify where training data is saved and specify the corresponding filenames
+# Define path to training data
+training_data_path = './data/training'
+
+# Get list of files in the data directory (only include ones ending with .csv)
+training_files = sorted([fn for fn in os.listdir(training_data_path) if fn.endswith('.csv')])
+
+# Load the training files into pandas dataframes
+training_data_dfs = [pd.read_csv(os.path.join(training_data_path, fn)) for fn in training_files]
+
+# --- Data loading and processing
+# Initialize the data manager
+fdm = FlowDataManager(
+    data_file_names=training_files,
+    data_file_type=None,  # Is inferred from the filename ending of the 1st file in the 'training_files' list
+    data_file_path=training_data_path,
+    save_path=save_path_data_handling,
+    verbosity=1
+    )
+    
+# Load data into memory
+print ('loading data...')
+# Data samples are now stored not in a Pandas DataFrames, but in a list of AnnData object.
+# This list is an attribute of the FlowDataManager class and can be accessed via FlowDataManager.anndata_list_.
+# AnnData is a Python class similar to Pandas DataFrames but with more options and functions for data annotation.
+# see: https://anndata.readthedocs.io/en/stable/
+fdm.load_data_files_to_anndata()
+
+# --- Check the number of events per sample
+# Create a dataframe with the columns sample and n_events, this df is an attribute of the FDM instance
+# and can be accessed via FDM.sample_sizes_. The df is stored as a global variable 'sample_sizes_df'.
+fdm.check_sample_sizes()
+sample_sizes_df = fdm.sample_sizes_
+
+# --- Apply preprocessing transformation to each sample
+# Example 1: Apply arcsinh with cofactor 150,
+# Example 2: Apply log transformation with custom cutoffs
+# In both cases, store non-transformed data in a separate layer of the AnnData object that we call 'no_trafo'.
+trafo_arcsinh = trafo_arcsinh # Set to True to apply arcsinh transformation, set to False to apply log transformation with custom cutoffs
+if trafo_arcsinh:
+    preprocessing_kwargs = {'cofactor': 150}
+    fdm.sample_wise_preprocessing(flavour='arcsinh', save_raw_to_layer='no_trafo', **preprocessing_kwargs)
+else:
+    # Define python dictionary mapping channel names to cutoffs (arbitrarily chosen here, adjust if needed)
+    # Optional: 'FS INT' and 'SS INT' will be transformed by division by 350000 instead of log
+    channel_name_to_cutoff = {
+        'FS INT': 50000, 'SS INT': 10000, '15-FITC': 100, '13-PE': 300, '33-PC7': 200, '2-APC': 200, '7-APC-AF700': 200, 
+         '34-ECD': 200, '117-PC5.5': 200, 'HLADR-PB': 200, '45-CO': 200,
+    } #  '-APC-AF750': 200,
+    preprocessing_kwargs = {'cutoffs': channel_name_to_cutoff}
+    fdm.sample_wise_preprocessing(
+        flavour='log10_w_custom_cutoffs', save_raw_to_layer='no_trafo', **preprocessing_kwargs
+        )
+    
+    # Apply division by 350000 transformation to 'FS INT' and 'SS INT' channels
+    # for adata in fdm.anndata_list_:
+    #  if 'FS INT' in adata.var_names:
+    #       adata[:, 'FS INT'].X = adata[:, 'FS INT'].X / 350000
+    #    if 'SS INT' in adata.var_names:
+    #       adata[:, 'SS INT'].X = adata[:, 'SS INT'].X / 350000
+
+# --- Downsample each sample to a target number of events
+fdm.sample_wise_downsampling(data_set='all', target_num_events=size_per_sample)
+
+# --- Extract concatenated data matrix for model training
+# Define channels to be used for model training
+channels = trainchannels
+
+# Extract the processed data matrices from the AnnData objects
+data_matrices = [adata[:, channels].X for adata in fdm.anndata_list_]
+# Get number of events per test sample and compute indices at which samples start in the concatenated data matrix
+num_events = [x.shape[0] for x in data_matrices]
+starting_indices = np.cumsum([0, ] + num_events)
+# Concatenate
+x_train = np.concatenate(data_matrices, axis=0)
+
+# Generate shuffle version of the training data
+idx_shuffle = np.random.permutation(x_train.shape[0])
+x_train_shuffled = x_train[idx_shuffle, :].copy()
+
+time_b = datetime.now()
+timeload = time_b - timestart
+
+# --- SOM training
+print('training SOM model...')
+# Instantiate the SOMClassifier, set hyperparameters
+som_clf = SOMClassifier(
+    som_topology='planar',
+    som_grid_type='rectangular',
+    som_dimensions=SOM_dim,
+    neighborhood='gaussian',
+    gaussian_neighborhood_sigma=0.1,
+    initialization='pca',
+    n_epochs=SOM_epochs,
+    radius_0=-0.25,
+    radius_n=0.1,
+    radius_cooling='exponential',
+    learning_rate_0=0.1,
+    learning_rate_n=0.001,
+    learning_rate_decay='exponential',
+    unlabeled_label=-999,
+    verbosity=1
+)
+
+# Model fitting. Since no labels are available only the SOM component is trained in an unsupervised fashion.
+# Still, a dummy label vector must be passed containing the label chosen to indicate unlabeled events
+y_dummy = np.ones(x_train.shape[0]) * -999
+
+# Paul: Use shuffled training data for model fitting
+som_clf.fit(X=x_train_shuffled, y=y_dummy)
+
+# Save the trained model
+som_clf.save(filename='som_classifier.pkl', filepath=save_path)
+
+# Paul: For in computing dimensionality reductions use unshuffled data for clean data export
+_, x_som, _, _ = som_clf.transform(x_train)
+
+time_c = datetime.now()
+timesom = time_c - time_b
+
+# --- t-SNE
+print ('computing t-SNE...')
+tsne_model = TSNE(n_components=2, n_jobs=-1, verbose=True)
+x_tsne = tsne_model.fit(x_train)
+# pickle.dump(x_tsne, open(os.path.join(save_path, 'tsne_embedding.pkl'), 'wb'))
+
+time_d = datetime.now()
+timeSNE = time_d - time_c
+
+# --- UMAP
+# print ('computing UMAP...')
+# umap_model = UMAP(n_components=2, n_jobs=-1)
+# x_umap = umap_model.fit_transform(x_train)
+
+# --- PARC clustering
+print('computing PARC clustering...')
+Parc1 = parc.PARC(x_train, jac_std_global='median')
+Parc1.run_PARC() # run the clustering
+parc_labels = Parc1.labels
+
+# Change back into sample-wise format (input format required by export function)
+x_soms_1 = [x_som[starting_indices[i]: starting_indices[i + 1], 0] for i in range(len(num_events))]
+x_soms_2 = [x_som[starting_indices[i]: starting_indices[i + 1], 1] for i in range(len(num_events))]
+# x_umaps_1 = [x_umap[starting_indices[i]: starting_indices[i + 1], 0] for i in range(len(num_events))]
+# x_umaps_2 = [x_umap[starting_indices[i]: starting_indices[i + 1], 1] for i in range(len(num_events))]
+x_tsnes_1 = [x_tsne[starting_indices[i]: starting_indices[i + 1], 0] for i in range(len(num_events))]
+x_tsnes_2 = [x_tsne[starting_indices[i]: starting_indices[i + 1], 1] for i in range(len(num_events))]
+parc_1 = [parc_labels[starting_indices[i]: starting_indices[i + 1]] for i in range(len(num_events))]
+
+# Export to FCS
+export_to_fcs(
+    data_list=fdm.anndata_list_,  # Export the test samples
+    layer_key='no_trafo',  # We want to export non-transformed data => choose the 'no_trafo' layer
+    sample_wise=False,  # Export one FCS in which the test samples are concatenated
+    add_columns=[
+        x_soms_1, x_soms_2,
+        x_tsnes_1, x_tsnes_2,
+        parc_1
+    ],  # Add columns corresponding to the 1st and 2nd dimension of the dimensionality reductions into 2D
+    add_columns_names=['SOM_1', 'SOM_2',  'TSNE_1', 'TSNE_2', 'PARC_labels'],  # Add names for added columns
+    scale_columns=['SOM_1', 'SOM_2',  'TSNE_1', 'TSNE_2', 'pop1', 'PARC_labels'],  # Select added columns for scaling
+    val_range=(0, 2**18),  # Range to which selected columns are scaled to
+    save_path=save_path,
+    save_filenames=f'train_w_PARC_{date_time_str}.fcs'
+)
+timetotal = datetime.now()-timestart
+with open(os.path.join(save_path, f'csv_training_{date_time_str}.txt'), 'a') as f:
+    f.write(f'"training_files and cell numbers" {date_time_str} \n')
+    for index, row in sample_sizes_df.iterrows():
+        f.write(f'"{row["sample"]}": {row["n_events"]}\n')
+    f.write(f'"training channels": {trainchannels}\n')
+    f.write(f'"samplesize max": {size_per_sample}\n')
+    f.write(f'"SOM_dim": {SOM_dim}\n')
+    f.write(f'"SOM_epochs": {SOM_epochs}\n')
+    f.write(f'"time data load": {timeload}\n')
+    f.write(f'"timesom": {timesom}\n')
+    f.write(f'"timeSNE": {timeSNE}\n')
+    f.write(f'"timetotal": {timetotal}\n')
