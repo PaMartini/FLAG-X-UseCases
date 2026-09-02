9579b6cef819a8eaa0aeb0b94d6fdfcde2ddec43
emlnr <eszter.molnar@fau.de>
2026-09-02 21:17:07 +0200

    fixed file naming, added filtering step for duplicate event IDs

--- concatenate_csv.py
+++ concatenate_csv.py
@@ -1,5 +1,5 @@
 # Concatenate CSV files for supervised training
-# Last modified: 25-08-2026
+# Last modified: 02-09-2026
 
 # Reads a list of CSV files from input directory, concatenates all into a single dataframe and exports as CSV.
 # Input (read from YAML config file):
@@ -12,6 +12,7 @@ import yaml
 from glob import glob
 import pandas as pd
 from pprint import pprint
+from collections import Counter
 import csv
 
 
@@ -33,7 +34,7 @@ print("")
 print("Number of CSV files found in input directory: " + str(len(path_list)))
 
 df_list = []
-filename_list = []
+filename = []
 population_dict = {}
 
 for file in sorted(path_list):
@@ -42,7 +43,7 @@ for file in sorted(path_list):
     format_en = False
     with open(file, 'r') as f:
         first_line = f.readline()
-        if first_line.find(",") != -1:
+        if first_line.count(",") > first_line.count(";"):
             format_en = True
         
     # Read in the file
@@ -51,20 +52,23 @@ for file in sorted(path_list):
     else:
         df = pd.read_csv(file, decimal=",", sep=";", low_memory=False)
 
-    # Get sample name, add as column if no sample_id column present
+    # Get sample and population name, add as column if no sample_id column present
     fname = file.split("/")[-1].split(".")[0]
     fname = fname.replace(" ", "_")
-    filename_list.append(fname)
-    if "sample_ID" not in df.columns:
-        df.insert(0, "sample_name", fname)
-
-    # Get population name, add as column
     pop_name = fname.split("_")[-1]   # assume the last part of the file name to be the population name
+    fname = fname[0:fname.rfind("_")]
+    if fname not in filename:
+        filename.append(fname)
+
+    # Add population column
     if pop_name not in population_dict:
         population_dict[pop_name] = len(population_dict)
     df.insert(len(df.columns), "population", population_dict[pop_name])
 
     df_list.append(df)
+
+if len(filename) != 1:
+    print("--> Inconsistencies found between input file names, please check input")
 
 print("Successfully imported: " + str(len(df_list)))
 print("")
@@ -87,7 +91,16 @@ for df in df_list[1:]:
 result = pd.concat(dfs_concat)
 if not cols_same:
     print("--> Warning: some of the input files have missing or additional columns, these files are excluded from the result.")
-    
+
+# --- Remove duplicate events
+result_dupl = result.sort_values("population")
+result_dupl["duplicated"] = result_dupl.duplicated(subset=["Event ID"])
+result_dupl = result_dupl[result_dupl["duplicated"] == True]
+result_filtered = result.sort_values("population").drop_duplicates("Event ID").sort_index()
+
+if result_dupl.shape[0] > 0:
+    print("Duplicate event IDs found: ", len(result_dupl["Event ID"]), " out of ", result.shape[0], " events")
+    print(result_dupl["Event ID"])
 
 
 
@@ -95,14 +108,8 @@ if not cols_same:
 
 print("Exporting results to: " + OUT)
 
-fn_roots = [fn.rsplit("_", 1)[0] for fn in filename_list]
-name = max(fn_roots, key=len)
-for fn in fn_roots:
-    for i in range(len(fn)):
-        if max(fn_roots, key=len)[i] != fn[i]:
-            name = name[:i] + "x" + name[(i+1):]
-
-result.to_csv(os.path.join(OUT, f"{name}_concat.csv"), sep=",", decimal=".", index=False)
+name = Counter(filename).most_common(1)[0][0]
+result_filtered.to_csv(os.path.join(OUT, f"{name}_concat.csv"), sep=",", decimal=".", index=False)
 
 # Save population name to ID mapping as CSV
 with open(os.path.join(OUT, f"{name}_populations.csv"), 'w', newline='') as f:
